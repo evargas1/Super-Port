fc1d12b1a8145134f6cb2a96f800c746d67b436e
Estrella Ms <estrellams@Estrellas-MacBook-Pro.local>
2021-12-14 19:41:52 -0600

    having

--- port/views.py
+++ port/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     context = {}
-    return render(request, 'store/main.html', context)
+    return render(request, 'port/main.html', context)
